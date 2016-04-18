da0fbfff1112f9bb0e9ca4f9e5a6b35247f3e2d3
FrancoisBillant <fbillant@gmail.com>
2016-04-18 21:09:34 +0200

    EcranService: Ajout methode SupprimerService

--- IHMConsole/EcranService.cs
+++ IHMConsole/EcranService.cs
@@ -19,7 +19,7 @@ namespace GestionEmployes.IHMConsole
                 Console.WriteLine("--- Gestion Des Services ---");
                 Console.WriteLine();
                 //Console.WriteLine("1. Sous-Sous-menu");
-                listeService();
+                ListeService();
                 Console.WriteLine();
                 Console.WriteLine("--- Menu ---");
                 Console.WriteLine("Edition: (A)jouter - (S)upprimer - (M)odifier... ToDo");
@@ -31,6 +31,9 @@ namespace GestionEmployes.IHMConsole
                         //tempsm1 = ssm(); //Appeler sous-sous-menu
                         AjouterService();
                         break;
+                    case ConsoleKey.S:
+                        tempsm1 = SupprimerService();
+                        break;
                     case ConsoleKey.D1:
                     case ConsoleKey.NumPad1:
                         tempsm1 = ssm();
@@ -41,23 +44,59 @@ namespace GestionEmployes.IHMConsole
             return tempsm1;
         }
 
-/*
-        public delegate void TrucAVerifier(string var);
-
-        public static void code(string codeAVerifier)
+        private ConsoleKeyInfo SupprimerService()
         {
-            Console.WriteLine("Code A Verifier" + codeAVerifier);
+            ConsoleKeyInfo tempssm = new ConsoleKeyInfo();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("--- Quel service voulez-vous supprimer  ---");
+                Console.WriteLine();
+                ListeService();
+                Console.WriteLine();
+                Console.WriteLine("R. Retour au sous menu précédent");
+                Console.WriteLine("P. Menu principal");
+                Console.WriteLine("Esc. Quitter");
+                tempssm = Console.ReadKey(true);
+
+                try
+                {
+                    List<Service> liste = MgrService.getInstance().Serviceslist;
+                    liste.Sort();
+                    int index = (int)(char.GetNumericValue(tempssm.KeyChar)) - 1;
+                    Console.WriteLine("Vous allez supprimer " + liste[index]);
+                    Console.WriteLine("Etes-vous sûr(e) ? (Y/N)");
+                    ConsoleKeyInfo reponse = Console.ReadKey();
+                    if (reponse.Key == ConsoleKey.Y)
+                    {
+                        MgrService.getInstance().SupprimerService(liste[index]);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            } while (!(tempssm.Key == ConsoleKey.Escape || tempssm.Key == ConsoleKey.P || tempssm.Key == ConsoleKey.R));
+            return tempssm;
         }
 
-        private static void Verifier(TrucAVerifier truc)
-        {
-            //bonjour.Invoke();
-            truc();
-        }
-*/
+        /*
+                public delegate void TrucAVerifier(string var);
+
+                public static void code(string codeAVerifier)
+                {
+                    Console.WriteLine("Code A Verifier" + codeAVerifier);
+                }
+
+                private static void Verifier(TrucAVerifier truc)
+                {
+                    //bonjour.Invoke();
+                    truc();
+                }
+        */
 
 
-         //TODO: Verfification de la validité à chaque etape de saisie (code puis libelle) - Délégué ?
+        //TODO: Verfification de la validité à chaque etape de saisie (code puis libelle) - Délégué ?
         private void AjouterService()
         {
             Console.Clear();
@@ -82,7 +121,7 @@ namespace GestionEmployes.IHMConsole
             } while (!(codeAVerifier=="A" || libelleAVerifier=="A"));
         }
         
-        private void listeService()
+        private void ListeService()
         {
             MgrService mgr = MgrService.getInstance();
             List<Service> liste = mgr.Serviceslist;
