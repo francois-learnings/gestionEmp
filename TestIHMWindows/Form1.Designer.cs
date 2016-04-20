48504a01b9833ca789a264b081dd27b5666aa288
FrancoisBillant <fbillant@gmail.com>
2016-04-20 13:28:02 +0200

    Ajout Projet TestIHMWindows

---
+++ TestIHMWindows/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TestIHMWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btValider = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblReponseALaQuestion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btValider
+            // 
+            this.btValider.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btValider.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btValider.Location = new System.Drawing.Point(161, 114);
+            this.btValider.Name = "btValider";
+            this.btValider.Size = new System.Drawing.Size(179, 23);
+            this.btValider.TabIndex = 0;
+            this.btValider.Text = "Valider";
+            this.btValider.UseVisualStyleBackColor = true;
+            this.btValider.Click += new System.EventHandler(this.btValider_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(75, 118);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(45, 187);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "nomCol";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "PrenomCol";
+            this.Column2.Name = "Column2";
+            // 
+            // lblReponseALaQuestion
+            // 
+            this.lblReponseALaQuestion.AutoSize = true;
+            this.lblReponseALaQuestion.Location = new System.Drawing.Point(354, 57);
+            this.lblReponseALaQuestion.Name = "lblReponseALaQuestion";
+            this.lblReponseALaQuestion.Size = new System.Drawing.Size(35, 13);
+            this.lblReponseALaQuestion.TabIndex = 3;
+            this.lblReponseALaQuestion.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 399);
+            this.Controls.Add(this.lblReponseALaQuestion);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btValider);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btValider;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label lblReponseALaQuestion;
+    }
+}
+
---
+++ TestIHMWindows/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestIHMWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btValider_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("J'ai cliquer sur le boutton");
+            MessageBox.Show("Coucou !");
+            DialogResult dr = MessageBox.Show("Ca va faire tout noir !",
+                                                "Titre de ma boite",
+                                                MessageBoxButtons.YesNo,
+                                                MessageBoxIcon.Exclamation);
+            Console.WriteLine(dr);
+            if (dr==DialogResult.Yes)
+            {
+                MessageBox.Show("On continue ?!");
+                this.lblReponseALaQuestion.Text = "Oui";
+            }
+            else if (dr==DialogResult.No)
+            {
+                MessageBox.Show("ok on arrète !");
+                this.lblReponseALaQuestion.Text = "Non";
+            }
+        }
+    }
+}
